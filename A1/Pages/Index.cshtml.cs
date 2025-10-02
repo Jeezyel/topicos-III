d26e265b640d6aeae57bd56a6348af12ea516bb4
Jeezyel <jeezyelpereira@gmail.com>
2025-10-02 12:06:21 -0300

    tela de cardarpio

--- A1/Pages/Index.cshtml.cs
+++ A1/Pages/Index.cshtml.cs
@@ -1,20 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using A1.Data;
+using A1.Models;
 
 namespace A1.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly A1.Data.A1Context _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(A1.Data.A1Context context)
         {
-            _logger = logger;
+            _context = context;
         }
+
+        public IList<ItemCarrinho> ItemCarrinho { get;set; } = default!;
+        public List<ItemCardapio> Cardapio { get; set; }
+
+        /*public async Task OnGetAsync()
+        {
+            ItemCarrinho = await _context.ItensCarrinho
+                .Include(i => i.Carrinho)
+                .Include(i => i.ItemCardapio).ToListAsync();
+        }*/
 
         public void OnGet()
         {
-
+            // Aqui você pode buscar os itens do banco de dados
+            Cardapio = _context.ItemCardapio.ToList(); // Supondo que você tenha um DbSet<ItemCardapio> no seu contexto
         }
     }
 }
--- A1/Pages/User/Index.cshtml.cs
+++ A1/Pages/User/Index.cshtml.cs
@@ -1,13 +1,8 @@
-﻿using A1.Data;
+﻿
 using A1.Models;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace A1.Pages.User
 {
