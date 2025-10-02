da26868d2952e2e87c0c70944335805bd2cafb5b
Jeezyel <jeezyelpereira@gmail.com>
2025-10-02 20:04:00 -0300

    criando na migration o usuario padrão admin@admin.com senha : u4KurkkizVRvCwH@

--- A1/Data/A1Context.cs
+++ A1/Data/A1Context.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using A1.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using A1.Models;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace A1.Data
 {
@@ -36,9 +37,53 @@ namespace A1.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            // Índice único que você já tinha
             modelBuilder.Entity<SugestaoDiaria>()
                 .HasIndex(s => new { s.Data, s.Periodo })
                 .IsUnique();
+
+            // Criação de roles
+            modelBuilder.Entity<IdentityRole>().HasData(
+                new IdentityRole
+                {
+                    Id = "f4dbf4dd-1df8-4e6a-9a15-abc123456789", // GUID fixo
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Id = "3b14a6f1-ef0b-4c89-b6b0-def987654321",
+                    Name = "Usuario",
+                    NormalizedName = "USUARIO"
+                }
+            );
+
+            // Usuário administrador
+            modelBuilder.Entity<Usuario>().HasData(
+                new Usuario
+                {
+                    Id = "1e8e011d-7fde-4b16-8078-6775d7fca57e",
+                    UserName = "admin@admin.com",
+                    NormalizedUserName = "ADMIN@ADMIN.COM",
+                    Email = "admin@admin.com",
+                    NormalizedEmail = "ADMIN@ADMIN.COM",
+                    EmailConfirmed = true,
+                    Nome = "Administrador", // seu campo extra
+                    PasswordHash = "AQAAAAIAAYagAAAAENxiM+eS0Ag9KL6O40a1TEUpV+jH0nxCFioLIPdrOJ9Y5x2Sx28OaWLn8dHwCML5nQ==",
+                    SecurityStamp = "d6f5c999-46de-4a7f-9c67-123456789abc", // <<< valor fixo
+                    ConcurrencyStamp = "c3aef999-96de-4a7f-9c67-abcdef123456" // <<< valor fixo
+                }
+            );
+
+            // Relaciona o usuário Admin com a Role Admin
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    UserId = "1e8e011d-7fde-4b16-8078-6775d7fca57e",
+                    RoleId = "f4dbf4dd-1df8-4e6a-9a15-abc123456789"
+                }
+            );
         }
     }
 }
